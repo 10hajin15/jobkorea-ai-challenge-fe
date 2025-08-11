c95975abcfc1342004b510d335433447d71f5c33
이하진 <hajin1015@naver.com>
2025-08-11 15:09:52 +0900

    feat: 공통 검색창 구현

---
+++ src/components/common/SearchInput.tsx
@@ -0,0 +1,41 @@
+import searchIcon from '@/assets/icons/SearchIcon.svg';
+import ClosedIcon from '@/components/common/icons/ClosedIcon';
+import { useState, type ChangeEvent } from 'react';
+
+interface PSearchInput {
+  placeholder?: string;
+  onSearch: (value: string) => void;
+}
+
+const SearchInput = ({ placeholder = '검색어를 입력해주세요.', onSearch }: PSearchInput) => {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+    onSearch(newValue);
+  };
+
+  return (
+    <div className="border-gray-3 flex h-[42px] w-full items-center gap-[10px] rounded-[4px] border px-[16px]">
+      <input
+        type="text"
+        placeholder={placeholder}
+        className="text-detail min-w-0 flex-1"
+        value={value}
+        onChange={handleChange}
+      />
+      <div className="h-[24px] w-[24px] shrink-0">
+        {value ? (
+          <button onClick={() => setValue('')} aria-label="clear">
+            <ClosedIcon width={20} height={20} color="#D2D2D2" />
+          </button>
+        ) : (
+          <img className="h-[24px] w-[24px]" src={searchIcon} alt="search" />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SearchInput;

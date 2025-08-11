e2726551195aa161c8f1a5ff4969d7cf81f6503e
이하진 <10hajin15@gmail.com>
2025-08-12 07:58:25 +0900

    feat: 결과 버튼 컴포넌트 구현

---
+++ src/components/common/SearchResultButton.tsx
@@ -0,0 +1,11 @@
+const SearchResultButton = () => {
+  return (
+    <div className="border-gray-3 h-[64px] border-t px-[24px] py-[8px]">
+      <button className="bg-primary text-header h-full w-full rounded-[8px] text-white">
+        000건의 결과보기
+      </button>
+    </div>
+  );
+};
+
+export default SearchResultButton;

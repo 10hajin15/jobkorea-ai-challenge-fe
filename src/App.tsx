ab34d4f24631388e80fa2fea8bf887dcd70ac853
이하진 <10hajin15@gmail.com>
2025-08-13 19:33:12 +0900

    feat: App 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import FilterPage from '@/pages/FilterPage';
+import { TabProvider } from '@/components/common/Tab/TabContext';
+
 function App() {
-  return <div></div>;
+  return (
+    <TabProvider>
+      <FilterPage />
+    </TabProvider>
+  );
 }
 
 export default App;
